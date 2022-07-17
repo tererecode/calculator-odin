8f94dc9a396b9591101aefb7e2759540842e70f4
gustavo <gus09ayala@gmail.com>
2022-07-16 20:15:05 -0400

    First version of calculator Calculator now kinda works, still missing features like: *operator runs calculation, only = work *back btn not implemented yet *changing operator before second input not working *Keyboard support not implemented

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
+const calcDisplay = document.getElementById('display');
+const clearBtn = document.getElementById('clear-btn');
+const calculateBtn = document.getElementById('equals-btn');
+const numBtns = document.querySelectorAll('.calc-btns-num');
+const operatorBtns = document.querySelectorAll('.calc-btn-operator');
+
+let userInput = '';
+let calledOperator;
+let previousInput;
+let lastResult;
+
+numBtns.forEach(num => num.addEventListener('click', populateDisplay))
+operatorBtns.forEach(operator => operator.addEventListener('click', nextUserInput))
+calculateBtn.addEventListener('click', calculate)
+clearBtn.addEventListener('click', clearCalc)
+
+function populateDisplay(event) {
+    userInput += event.target.textContent
+    calcDisplay.textContent = userInput
+    console.log('User pressed ', event.target.textContent, 'userInput val ', userInput)
+}
+
+function nextUserInput(event) {
+    if (calledOperator == undefined) {
+        calledOperator = event.target.textContent;
+        console.log('new operator is ', calledOperator, 'previous is', previousInput)
+    }
+    if (previousInput == undefined || previousInput == '') {
+        previousInput = userInput
+        userInput = ''
+        console.log('operator is ', calledOperator, 'previous is', previousInput)
+    }
+}
+
+function calculate() {
+    lastResult = operate(calledOperator, Number(previousInput), Number(userInput))
+    console.log('result is ', lastResult)
+    calcDisplay.textContent = lastResult
+    previousInput = lastResult
+    userInput = ''
+    calledOperator = undefined;
+
+}
+
 function add(a, b) {
     return a + b
 };
@@ -11,7 +55,13 @@ function multiply(a, b) {
 };
 
 function divide(a, b) {
-    return a / b
+    if (b === 0) {
+        alert('Oops! don\'t break the universe!');
+        clearCalc();
+    } else {
+        return a / b
+    }
+
 };
 
 function operate(operator, a, b) {
@@ -29,4 +79,13 @@ function operate(operator, a, b) {
             return divide(a, b)
             break;
     }
+}
+
+function clearCalc() {
+    userInput = '';
+    calledOperator = undefined;
+    previousInput = undefined;
+    lastResult = undefined;
+    calcDisplay.textContent = '0';
+
 }
